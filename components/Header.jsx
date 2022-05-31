ca3035cc0aaf04ddca66a40ab515fd0d66dcd9db
Eli Guy <eliguy343@gmail.com>
2022-05-31 16:24:43 +0300

    Fixed: header links not workingn

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,36 +1,41 @@
 import React, {useContext} from 'react';
 import Link from 'next/link';
+
 const Categories = [
     {name:'React', slug:'react'},
     {name:'React-bs', slug:'react-bs'}
 ];
 
 const Header = () => {
-  //TODO: FIX links not showing up
+  console.log(Categories)
   return (
     <div 
-        style={{'background-color':"rgba(80,80,200,0.7)"}} 
+        style={{'backgroundColor':"rgba(80,80,200,0.7)"}} 
         className='container mx-auto px-10 mb-8'
     >
       <div className='border-b w-full inline-block border-blue-400 py-8'>
         <div className='md:float-left block'>
           <Link href='/'>
-              <span className='cursor-pointer font-bold-text-4xl text-white'>
+              <span
+                style={{"fontSize":"25px"}}
+                className='cursor-pointer font-bold-text-4xl text-white'>
                 Cooking Blog
               </span>
           </Link>
         </div>
-        <div className='hidden md:float-left md:contents'>
-          {Categories.map((category)=>{
+        <div className='md:float-left md:contents'>
+          {Categories.map((category)=>
               <Link key={category.slug} href={`/category/${category.slug}`}>
                 <span 
                   className='md:float-right mt-2 align-middle text-white
                     ml-4 font-semibold cursor-pointer'
                 >
-                  {category.name}
+                  <div className='linkButton'>
+                    {category.name}
+                  </div>
                 </span>
               </Link>
-          })}
+          )}
         </div>
       </div>
     </div>
