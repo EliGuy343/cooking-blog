1d6f0d42ea1c230c85678fbf35cd10a12c4cef16
Eli Guy <eliguy343@gmail.com>
2022-06-03 13:02:25 +0300

    added: author bio

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div>
-        <h1>Author</h1>
+    <div 
+      style={{'backgroundColor':"rgba(150,80,10,0.3)"}}
+      className='flex items-center text-center mt-20 mb-8 p-8 relative rounded-lg'
+    >
+        <img
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className='align-middle rounded-full'
+          src={author.photo.url}
+        />
+        <h3 className='ml-2 text-black text-xl font-bold'>
+          {author.name}
+        </h3>
+        <p className='mt-1 ml-12 font-bold break-words'>
+          {author.bio}
+        </p>
     </div>
   )
 }
